66a06863252d56d93f8d9cdee3e1f56afb7490e3
ibrahim <ibrahim.rafei.nr@gmail.com>
2024-09-09 15:42:30 +0200

    ssl certificate error showing

--- src/main/java/pageobject/LandingPage.java
+++ src/main/java/pageobject/LandingPage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import reuseable.AbstractClass;
 
+import javax.net.ssl.SSLHandshakeException;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 
@@ -28,10 +29,12 @@ public class LandingPage extends AbstractClass {
         driver.get(data);
         URL siteURL = new URL(data);
         WebElement body = driver.findElement(By.tagName("body"));
+        try {
+            // Attempt to open HTTP connection
+            HttpURLConnection connection = (HttpURLConnection) siteURL.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
 
-        HttpURLConnection connection = (HttpURLConnection) siteURL.openConnection();
-        connection.setRequestMethod("GET");
-        connection.connect();
         int responseCode = connection.getResponseCode();
         if (responseCode == 200) {
             Assert.assertFalse(driver.getTitle().contains("404") ||  driver.getTitle().isEmpty()|| body.getText().contains("404"), data + " is returning a 404 error." + "\n");
@@ -43,7 +46,13 @@ public class LandingPage extends AbstractClass {
         // Check the page title or any other element to determine if the page loaded correctly
 
        // Assert.assertFalse(!driver.getCurrentUrl().startsWith("https"), data + "is not SSL Certified");
-
+        } catch (SSLHandshakeException e) {
+            // Handle SSL-related exceptions (indicating HTTPS was expected)
+            Assert.fail(data + " SSL certificate not working" + "\n");
+        } catch (IOException e) {
+            // Handle other IO exceptions
+            e.printStackTrace();
+        }
 
         }
 
